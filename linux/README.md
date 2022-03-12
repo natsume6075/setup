f28f6a692039a1a217f9d285208348cdd01f7de3
natsume6075 <natsume6075@gmail.com>
2022-03-12 17:53:34 +0900

    curl一発で他リポジトリ含め完結するようにしたい。とりあえずライブラリのダウンロード&セットまで。

---
+++ linux/README.md
@@ -0,0 +1,16 @@
+# Linux 環境構築
+
+```bash
+curl -sSL https://raw.githubusercontent.com/natsume6075/setup/master/linux/setup.sh | bash
+```
+
+上のコマンドを実行すると、以下ができる。
+
+- lib/ の準備
+    - Dropbox を使うなら、Dropbox をセットアップする。
+    - そうでないなら ./lib を作成し、各ライブラリをリポジトリからダウンロードし、lib 配下に配置する。
+
+## その他やること
+
+echo set fish as default shell: chsh - which fish
+
--- linux/setup.sh
+++ linux/setup.sh
@@ -1,14 +1,57 @@
-#!/bin/sh
+#!/bin/bash
+AppToSetup=(dotfiles nvim fish bin)
 
-echo "Install many many apps? (y/[other])"
-read input
-if [ $input = 'y' ] ; then
+function GetConfirmation() {
+    echo "$1 (y/n)"
+    read input
+    if [ "$input" = 'y' ] ; then
+        return 1
+    elif [ "$input" = 'n' ] ; then
+        return 0
+    else
+        GetConfirmation "$1"
+    fi
+}
+
+function PrepareLibWithDropbox() {
+    dropbox start -i
+    LIB_DIRECTORY="~/Dropbox/lib"
+}
+
+function PrepareLibWithoutDropbox() {
+    if GetConfirmation "現在のディレクトリ直下に lib/ を作成してよろしいですか？" ; then
+        echo "Abort setup."
+        exit 0
+    fi
+    LIB_DIRECTORY="./lib"
+    mkdir lib
+
+    # Download all libraries included in AppToSetup from my repositories.
+    for app in "${AppToSetup[@]}"
+    do
+        curl -sSLo $app.tar.gz https://github.com/natsume6075/$app/tarball/master
+        mkdir $LIB_DIRECTORY/$app
+        tar -zxf $app.tar.gz --strip-components 1 -C $LIB_DIRECTORY/$app
+        rm $app.tar.gz
+    done
+}
+
+
+# If necessary, install apps by apt.
+if ! GetConfirmation "Install many many apps by apt?" ; then
     sudo apt update
     sudo apt install -y fish zsh neovim tmux tree xdg-utils zip nautilus-dropbox
-
-    # Install daemon and start dropbox, open URL to link Dropbox account.
-    dropbox start -i
 fi
 
-echo set fish as default shell: chsh - which fish
+# Download the library via Dropbox or curl, then set LIB_DIRECTORY.
+if ! GetConfirmation "Download the library for setup. Use Dropbox?" ; then
+    PrepareLibWithDropbox
+else
+    PrepareLibWithoutDropbox
+fi
+
+for app in "${AppToSetup[@]}"
+do
+    echo $app
+done
 
